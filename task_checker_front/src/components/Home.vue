01c0e249cd0b53591fc67289cabc840e5992aad6
igachan-lakeel <igaue-re@lakeel.com>
2026-03-04 11:24:48 +0900

    first commit

---
+++ task_checker_front/src/components/Home.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h2>Hello world</h2>
+</template>
+
+<style scoped></style>
---
+++ task_checker_front/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App); // App.vue をアプリとして登録
+app.mount("#app"); // index.html の id="app" の要素に表示
