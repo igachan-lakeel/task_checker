19785d57d889237433cc471cb354bf99b268b783
igachan-lakeel <igaue-re@lakeel.com>
2026-03-04 13:28:54 +0900

    大枠のレイアウトを実装

--- task_checker_front/src/main.js
+++ task_checker_front/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@/assets/main.css"; // グローバルCSSを追加
 
 const app = createApp(App); // App.vue をアプリとして登録
 app.mount("#app"); // index.html の id="app" の要素に表示
