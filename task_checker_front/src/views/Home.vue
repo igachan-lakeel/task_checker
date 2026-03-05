87d05ec535653aa0a8eb6f1d11eead5c1531e0cb
igachan-lakeel <igaue-re@lakeel.com>
2026-03-05 14:46:45 +0900

    ジャンル選択部分のマークアップ

--- task_checker_front/src/views/Home.vue
+++ task_checker_front/src/views/Home.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import Header from "@/components/common/Header.vue";
+import Select from "@/components/ui/Select.vue";
+import PlusCircleOutline from "vue-material-design-icons/PlusCircleOutline.vue";
+</script>
 
 <template>
-  <div class="main"></div>
+  <div class="main">
+    <Header />
+    <div class="genre">
+      <Select />
+      <PlusCircleOutline class="add-icon" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -9,5 +19,18 @@
   width: 100vw;
   height: 100vh;
   background-color: #f6f8f9;
+}
+
+.genre {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  padding-top: 20px;
+}
+
+.add-icon {
+  margin-left: 10px;
+  color: rgb(70, 70, 70);
 }
 </style>
