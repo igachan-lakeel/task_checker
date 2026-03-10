7d7da7c79c78ac7fe3f0bf3b6ebdba6560df0724
igachan-lakeel <igaue-re@lakeel.com>
2026-03-10 16:31:37 +0900

    検索結果ページの作成

---
+++ task_checker_front/src/components/SearchResult.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from "./Header.vue";
+</script>
+
+<template>
+  <div class="main">
+    <Header />
+    <div class="contents"></div>
+  </div>
+</template>
+
+<style scoped></style>
